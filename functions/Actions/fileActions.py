b244486adde4327c19faceaeeea0d9ec87444488
imvg <tongtibagf@gmail.com>
2021-01-03 10:14:55 +0800

    2021-01-03

--- functions/Actions/fileActions.py
+++ functions/Actions/fileActions.py
@@ -15,33 +15,34 @@ from .FormatTypes.pdfsplit import pdfsplit
 
 
 def auth_token(ctype, token, client):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f'select status from functions_conversionlog where token="{token}";')
-        res = cursor.fetchone()
-        if res:
-            if int(res[0]) == 0:
-                cursor.execute(f'UPDATE functions_conversionlog SET ctype="{ctype}", status=1, time="{int(time.time())}", client="{client}" where token="{token}"')
-                return True
-            else:
-                print(f"Token已使用 {token}")
-                return False
-        else:
-            cursor.execute(f'select status from functions_admintoken where token="{token}";')
-            admin_res = cursor.fetchone()
-            if admin_res:
-                if int(admin_res[0]) == 0:
-                    cursor.execute(f'UPDATE functions_admintoken SET time="{int(time.time())}" where token="{token}"')
-                    return True
-                else:
-                    print(f"管理员Token已被禁用 {token}")
-                    return False
-            else:
-                print(f"Token不存在 {token}")
-                return False
-    except Exception as e:
-        print(f"Token处理异常 {e}")
-        return False
+    return True
+    # try:
+    #     cursor = connection.cursor()
+    #     cursor.execute(f'select status from functions_conversionlog where token="{token}";')
+    #     res = cursor.fetchone()
+    #     if res:
+    #         if int(res[0]) == 0:
+    #             cursor.execute(f'UPDATE functions_conversionlog SET ctype="{ctype}", status=1, time="{int(time.time())}", client="{client}" where token="{token}"')
+    #             return True
+    #         else:
+    #             print(f"Token已使用 {token}")
+    #             return False
+    #     else:
+    #         cursor.execute(f'select status from functions_admintoken where token="{token}";')
+    #         admin_res = cursor.fetchone()
+    #         if admin_res:
+    #             if int(admin_res[0]) == 0:
+    #                 cursor.execute(f'UPDATE functions_admintoken SET time="{int(time.time())}" where token="{token}"')
+    #                 return True
+    #             else:
+    #                 print(f"管理员Token已被禁用 {token}")
+    #                 return False
+    #         else:
+    #             print(f"Token不存在 {token}")
+    #             return False
+    # except Exception as e:
+    #     print(f"Token处理异常 {e}")
+    #     return False
 
 
 @require_http_methods(['POST'])
